b79d1023f8c0dfd5974a21282f76d660bfcbe919
unknown <thuythao1912@gmail.com>
2019-10-08 13:19:52 +0700

    split components

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 
 import ShrimpSelling from "./Components/Orders/ShrimpSelling";
 import ShrimpOrder from "./Components/Orders/ShrimpOrder";
+import SellList from "./Components/Market/SellList";
 
 function App() {
   return (
     <div className="App">
       <ShrimpSelling />
       <ShrimpOrder />
+      <SellList />
     </div>
   );
 }
