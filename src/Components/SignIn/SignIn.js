79c4f8b79e1bdb0872ffab9bc84b8c16b2608ba0
hao1710 <anhhaovo1710@gmail.com>
2019-10-10 15:03:31 +0700

    signInForm

--- src/Components/SignIn/SignIn.js
+++ src/Components/SignIn/SignIn.js
@@ -1,51 +1,77 @@
 import React, { Component } from "react";
 
-class SignIn extends Component {
-    render() {
-        return (
-           <div>
-               <div className="col-sm-6  bg-danger">
-                   <div>
-                       <h2 className="text-stfarm text-center"><strong>ĐĂNG NHẬP</strong></h2>
-                   </div>
-                   <div className="panel panel-default" style={{border: 'none'}}>
-                       <div className="panel-body">
-                           <form className="form-horizontal " action="/action_page.php">
-                               <div className="form-group">
-                                   <label className="control-label col-sm-3" htmlFor="email">Tên đăng nhập</label>
-                                   <div className="col-sm-6">
-                                       <input type="email" className="form-control" id="email" placeholder="Nhập vào email" name="email" />
-                                   </div>
-                                   <p style={{color: 'red'}}>(*)</p>
-                               </div>
-                               <div className="form-group">
-                                   <label className="control-label col-sm-3" htmlFor="pwd">Mật khẩu</label>
-                                   <div className="col-sm-6">
-                                       <input type="password" className="form-control" id="pwd" placeholder="Nhập vào mật khẩu" name="pwd" />
-                                   </div>
-                                   <p style={{color: 'red'}}>(*)</p>
-                               </div>
-                               <div className="form-group">
-                                   <div className="col-sm-offset-3 col-sm-9">
-                                       <p>Quên mật khẩu?</p>
-                                   </div>
-                               </div>
-                               <div className="form-group">
-                                   <div className="col-sm-offset-3 col-sm-9">
-                                       <button type="submit" className="btn btn-primary">Đăng nhập</button>
-                                   </div>
-                               </div>
-                               <div className="form-group">
-                                   <div className="col-sm-offset-3 col-sm-9">
-                                       <p>Chưa có tài khoản? <a href="#" style={{color: 'red'}}>Đăng kí ngay!</a></p>
-                                   </div>
-                               </div>
-                           </form>
-                       </div>
-                   </div>
-               </div>
-           </div>
 
+class SignIn extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            userName: '',
+            pwd: ''
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleChange(e) {
+        let target = e.target;
+        let value = target.type === 'checkbox' ? target.checked : target.value;
+        let name = target.name;
+
+        this.setState({
+            [name]: value
+        });
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        alert('Success');
+        // console.log('The form was submitted with the following data:');
+        // console.log(this.state);
+    }
+
+    render() {
+
+        return (
+            <form onSubmit={this.handleSubmit} className="FormFields" onSubmit={this.handleSubmit}>
+                <div className="bg-white col-sm-8 offset-lg-2 p-3">
+
+                    <div className="text-stfarm text-center">
+                        <div>
+                            <h2><strong>ĐĂNG NHẬP</strong></h2>
+                        </div>
+                    </div>
+
+                    <div className="form-group row">
+                        <label className="control-label col-sm-3" htmlFor="userName">Tên đăng nhập</label>
+                        <div className="col-sm-7">
+                            <input type="text" className="form-control" id="userName" name="userName" required value={this.state.userName} onChange={this.handleChange} />
+                        </div>
+                        <p className="text-danger">(*)</p>
+                    </div>
+                    <div className="form-group row">
+                        <label className="control-label col-sm-3" htmlFor="pwd">Mật khẩu</label>
+                        <div className="col-sm-7">
+                            <input type="password" className="form-control" id="pwd" name="pwd" required value={this.state.pwd} onChange={this.handleChange} />
+                        </div>
+                        <p className="text-danger">(*)</p>
+                    </div>
+                    <div className="form-group row">
+                        <div className="offset-lg-3 col-sm-7">
+                            <p><a href="#">Quên mật khẩu?</a></p>
+                        </div>
+                    </div>
+                    <div className="form-group row">
+                        <div className="offset-lg-3 col-sm-7">
+                            <button type="submit" className="btn btn-warning btn-lg btn-block" id="btnSignIn">Đăng nhập</button>
+                        </div>
+                    </div>
+                    <div className="form-group row">
+                        <div className="offset-lg-3 col-sm-7">
+                            <p>Chưa có tài khoản? <a href="#">Đăng ký ngay!</a></p>
+                        </div>
+                    </div>
+
+                </div >
+            </form>
         )
     }
 }
