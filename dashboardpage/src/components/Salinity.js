2db866e0c25235c8dba19ca236f0e0c847b9e1f3
unknown <thuythao1912@gmail.com>
2019-10-22 01:56:44 +0700

    dashboard layout

---
+++ dashboardpage/src/components/Salinity.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+class Salinity extends Component {
+  constructor(props) {
+    super(props);
+  }
+  //Funtions
+
+  render() {
+    return (
+      <div className="">
+        <h3>ĐỘ MẶN: HÀO</h3>
+      </div>
+    );
+  }
+}
+
+export default Salinity;
---
+++ dashboardpage/src/components/WaterSource.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+class WaterSource extends Component {
+  constructor(props) {
+    super(props);
+  }
+  //Funtions
+
+  render() {
+    return (
+      <div className="">
+        <h3>Nguồn nước: HÀO</h3>
+      </div>
+    );
+  }
+}
+
+export default WaterSource;
