08ea1baaeddded387ecd4418a958080605d4f3b5
lockhaicttv <32085956+lockhaicttv@users.noreply.github.com>
2019-10-28 19:57:43 +0700

    add Map

--- dashboardpage/src/components/Map.js
+++ dashboardpage/src/components/Map.js
@@ -1,15 +1,34 @@
 import React, { Component } from "react";
+import L from "leaflet";
+import 'leaflet/dist/leaflet.css';
+import styled from "styled-components";
+
+const Wrapped = styled.div`
+        width: ${props => props.width};
+        height: ${props => props.height};
+  `;
+
 class Map extends Component {
-  constructor(props) {
-    super(props);
+
+
+  componentDidMount() {
+    this.map = L.map('map',
+        {
+          center: [10.123, 105.13],
+          zoom: 13,
+          zoomControl: false
+        });
+
+    L.tileLayer( 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
+      maxZoom: 8,
+      maxNativeZoom: 8,
+      detectRetina: true,
+    }).addTo(this.map);
   }
-  //Funtions
 
   render() {
     return (
-      <div className="">
-        <h1>MAP: CHIẾN</h1>
-      </div>
+        <Wrapped id="map" width = "100%" height = "100%"/>
     );
   }
 }
